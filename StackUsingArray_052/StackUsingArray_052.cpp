5255de196fb15736229281b9161e4b137f66688a
adityapermono8 <adityapermono8@gmail.com>
2023-06-15 10:39:14 +0700

    menambahkan metod push di dalam class program

--- StackUsingArray_052/StackUsingArray_052.cpp
+++ StackUsingArray_052/StackUsingArray_052.cpp
@@ -7,4 +7,24 @@ class program {
 private:
 	string stack_array[5];
 	int top;
+
+public:
+	program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "Stack Full" << endl;
+			return "";
+		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+		cout << element << "ditambahkan(pushed). " << endl;
+
+		return element;
+	}
 };
